ad16e1fec6015434f0d784f1980770207b709da5
Carl Pulley <c.j.pulley@hud.ac.uk>
2013-09-08 01:01:15 +0100

    Multicast updates

--- cloud/src/main/scala/workflow/wrapper/multicast.scala
+++ cloud/src/main/scala/workflow/wrapper/multicast.scala
@@ -12,26 +12,27 @@ import org.apache.camel.scala.dsl.builder.RouteBuilder
 
 class ListAggregationStrategy extends AbstractListAggregationStrategy[String] {
   def getValue(exchange: Exchange): String = {
-    "FIXME"
+    exchange.getIn.getHeader("clientId").asInstanceOf[String]
   }
 }
 
 class Multicast(name: String, children: List[Workflow], timeout: Duration = 3 seconds) extends Workflow {
   def generateFeedback = { (exchange: Exchange) =>
-    // TODO:
+    // TODO: use Scala's HTML/XML processing?
+    val item_list = exchange.getIn.getBody.asInstanceOf[List[String]].reduceLeft((items, item) => s"$items</li>\n  <li>$item")
+    exchange.getIn.setBody(s"<ul>\n  <li>$item_list</li>\n</ul>")
   }
 
   def route = new RouteBuilder {
     "direct:%s".format(name) ==> {
       // FIXME: need to content enrich message using control buses message store
       errorHandler(deadLetterChannel("jms:queue:error"))
-      to("jms:queue:test")
       aggregate(header("clientId"), new ListAggregationStrategy()).completionTimeout(timeout.toMillis)
       process(generateFeedback)
     }
   
     for(child <- children) {
-      "jms:topic:%s".format(name) ==> {
+      s"jms:topic:$name" ==> {
         child.route
       }
     }
